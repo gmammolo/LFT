88138b146e0a324cdbf8dfe207815db902d79692
Giuseppe Mammolo <giuseppe.mammolo@hotmail.com>
2015-10-14 16:10:13 +0200

    -Add Es 3

--- src/LFT/A2015/Es1/Comma3.java
+++ src/LFT/A2015/Es1/Comma3.java
@@ -5,66 +5,48 @@
  */
 package LFT.A2015.Es1;
 
-import LFT.DFA.DFA;
 import LFT.DFA.DFAModel;
-import static LFT.Lab_24_10_2014.Es2_5.Es2_5.GenerateDFA;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
  * @author Giuseppe
  */
-public class Comma3 extends DFAModel{
-    
-    @Override
-    protected int NumState () {return 9; }
-           
-    @Override
-    public void InitializeDFA()
-    {
-        SetMove(0, '0','9', 1);
-        SetMove(0, new char[]{'+', '-'}, 6);
-        SetMove(0, '-', 6);
-        SetMove(0, '.', 5);
-        
-        SetMove(1, '0', '9', 1);
-        SetMove(1, 'e', 2);
-        SetMove(1, ' ', 8);
-        
-        SetMove(2, '0', '9', 3);
-        SetMove(2, new char[]{'+', '-'}, 4);
-        
-        SetMove(3, '0', '9', 3);
-        SetMove(3, ' ', 8);
-        
-        SetMove(4, '0', '9', 3);
-        
-        SetMove(5, '0', '9', 7);
-        
-        SetMove(7, '0', '9', 7);
-        SetMove(7, ' ', 8);
-        
-        SetMove(8, ' ', 8);
+public class Comma3 extends DFAModel {
 
-        AddFinalState(1);
-        AddFinalState(3);
-        AddFinalState(7);
-        AddFinalState(8);
+    @Override
+    protected int NumState() {
+        return 3;
     }
-    
+
+    @Override
+    public void InitializeDFA() {
+        
+        SetMove(0, '0', 0);
+        SetMove(0, '1', 1);
+        SetMove(1, '1', 0);
+        SetMove(1, '0', 2);
+        SetMove(2, '0', 1);
+        SetMove(2, '1', 2);
+        AddFinalState(0);
+    }
 
     public static void main(String[] args) throws Exception {
-        //InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        //BufferedReader reader = new BufferedReader(inputStreamReader);
-        //System.out.println("Type the line:");
-        //String line = reader.readLine();
 
-        //toJava("Es1_3");
-        new Comma3().toDot("Es1_3");
+        Comma3 es = new Comma3();
+        es.toJava("Comma3_Code");
+        es.toDotAlternative("Comma3_Dot");
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        System.out.println("Type the line:");
+        String line = reader.readLine();
+        if (line == null) {
+            System.out.println("Input non Accettabile");
+        } else {
+            System.out.println("Result: " + new Comma6().Scan(line));
+        }
 
     }
-    
+
 }
