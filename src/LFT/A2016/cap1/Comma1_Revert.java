1673d254ba326e13226e8d35e0cd528e35159daa
Giuseppe Mammolo <gmammolo@gmail.com>
2016-12-06 12:48:42 +0100

    -aggiunte esercitazioni 1.1, 1.2, 1.3 -BUG nel Minimize Comma3.java

--- src/LFT/A2016/cap1/Comma1_Revert.java
+++ src/LFT/A2016/cap1/Comma1_Revert.java
@@ -11,7 +11,7 @@ import main.java.com.opendfa.DFAModel;
  *
  * @author Giuseppe
  */
-public class Comma1 extends DFAModel {
+public class Comma1_Revert extends DFAModel {
 
     @Override
     protected int numState() {
@@ -31,12 +31,16 @@ public class Comma1 extends DFAModel {
 
         setMove(3, new char[]{'0', '1'}, 3);
 
-        addFinalState(3);
+        addFinalState(0);
+        addFinalState(1);
+        addFinalState(2);
+        addFinalState(-1);
+
     }
 
     public static void main(String[] args) throws Exception {
 
-        Comma1 comma = new Comma1();
+        Comma1_Revert comma = new Comma1_Revert();
 
         //InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         //BufferedReader reader = new BufferedReader(inputStreamReader);
@@ -44,11 +48,11 @@ public class Comma1 extends DFAModel {
         //String line = reader.readLine();
         //comma.scan(line);
         String outputDir = "/home/terasud/NetBeansProjects/LFT/src/LFT/A2016/cap1/";
-        System.out.println(comma.toJava("Es1Comma1_Code"));
-        comma.writeToJava("Es1Comma1_Code", outputDir);
-        System.out.println(comma.toDot("Es1Comma1_Dot"));
-        comma.writeToDot("Es1Comma1_Dot", outputDir);
-        comma.ToPng("Es1Comma1_Png", outputDir);
+        System.out.println(comma.toJava("Es1Comma1_Revert_Code"));
+        comma.writeToJava("Es1Comma1_Revert_Code", outputDir);
+        System.out.println(comma.toDot("Es1Comma1_Revert_Dot"));
+        comma.writeToDot("Es1Comma1_Revert_Dot", outputDir);
+        comma.ToPng("Es1Comma1_Revert_Png", outputDir);
     }
 
 }
